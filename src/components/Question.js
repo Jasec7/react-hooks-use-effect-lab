731f4368e88e4a30e5526bdc1313d875d70e3140
Jasec <jasec_7@hotmail.com>
2025-05-12 03:08:33 -0700

    complete react hooks use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+//import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+   const setResetTime = setInterval(() =>{setTimeRemaining(prevTime =>{
+    if(prevTime <= 1){
+      clearTimeout(setResetTime);
+      return 0;
+    }
+    
+    return prevTime - 1
+   })
+   
+  }, 1000);
+  
+    
+    return function(){
+      clearTimeout(setResetTime)
+    };
+ }, []);
+
+ useEffect(() => {
+  if (timeRemaining === 0) {
+    onAnswered(false);
+  }
+}, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
